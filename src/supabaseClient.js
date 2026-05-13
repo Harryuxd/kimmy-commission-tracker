31c604e688feef3ebc77eb5d254c568742d80bff
harryuxd <harryux@outlook.com>
2026-05-12 21:44:49 -0400

    Update Supabase project connection

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://iomqzagfoejqiybuagiq.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvbXF6YWdmb2VqcWl5YnVhZ2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYzNzU2MzksImV4cCI6MjA4MTk1MTYzOX0.3GyYCGkpVBhjDddkPZUi6T1hhvAhNB6CWhhKmokcwl4'
+const supabaseUrl = 'https://smfqoscvlmgnnomadcjb.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtZnFvc2N2bG1nbm5vbWFkY2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MjI1MDksImV4cCI6MjA5NDE5ODUwOX0.4f2rck0dnanLGg6Yq94DmnFDbgzCNhbLlOcWLwhjwYk'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
